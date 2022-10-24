75d758201e92d015f283af0b785dc0c51db14167
Dairo Camacho <dairoleandro83@gmail.com>
2022-10-24 16:37:31 -0500

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OrderComponent } from './order/order.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PersonComponent } from './person/person.component';
+import { ProductComponent } from './product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'person', component: PersonComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'order', component: OrderComponent },
+  { path: '', component: HomeComponent },
+  // { path : 'salir', redirectTo: '/home', pathMatch: 'full' }, 
+  // redirectTo: se usa para cuando usa servicios externos donde recibes algo (url) y lo redireccionas a una pagina
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
