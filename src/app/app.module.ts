bb87648a7318c9f666dc6e4e91dfeb2344f8044a
Dairo Camacho <dairoleandro83@gmail.com>
2022-10-24 16:29:57 -0500

    add bootstrap and 5 components. component home, person, product, order, page-not-found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PersonComponent } from './person/person.component';
+import { OrderComponent } from './order/order.component';
+import { ProductComponent } from './product/product.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PersonComponent,
+    OrderComponent,
+    ProductComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
